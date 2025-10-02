c3556bb3414a9ae56f12837f418a3d5819ffd919
goldySlav <g00L1v3rjkE@proton.me>
2025-10-02 06:52:53 +0200

    rebalance recipe: MEGA co-processor

--- kubejs/server_scripts/mods/megacells/megacells.js
+++ kubejs/server_scripts/mods/megacells/megacells.js
@@ -51,9 +51,9 @@ ServerEvents.recipes(event => {
 	])
 	event.recipes.extendedcrafting.shaped_table(MGC('mega_crafting_accelerator'), [
 		'CEBEC',
-		'EDDDE',
-		'BDADB',
-		'EDDDE',
+		'EDFDE',
+		'BFAFB',
+		'EDFDE',
 		'CEBEC',
 	], {
 		A: MGC('mega_crafting_unit'),
@@ -61,6 +61,7 @@ ServerEvents.recipes(event => {
 		C: CP('iron_2'),
 		D: KJ('engineering_processor_wafer'),
 		E: CP('fluix_1'),
+		F: AE2('crafting_accelerator'),
 	})
 	
 	//MEGA item storage cells
